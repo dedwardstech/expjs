15fb2907a55765ba8fe626b74464c4af4a285da8
David Edwards <davidedwards.tech16@gmail.com>
2021-07-24 16:27:45 -0400

    added Token interface and TokenType enum

---
+++ src/parse/token.ts
@@ -0,0 +1,31 @@
+export enum TokenType {
+    Err = 'error',
+    EOF = 'eof',
+
+    Identifier = 'identifier',
+
+    Number = 'number',
+    String = 'string',
+    Boolean = 'boolean',
+
+    LogicalAnd = 'logical-and',
+    LogicalOr = 'logical-or',
+    LogicalNot = 'logical-not',
+
+    LeftParent = 'left-paren',
+    RightParen = 'right-paren',
+
+    IsEqual = 'is-equal',
+    IsNotEqual = 'is-not-equal',
+    IsGreater = 'is-greater',
+    IsGreaterOrEqual = 'is-greater or equal',
+    IsSmaller = 'is-smaller',
+    IsSmallerOrEqual = 'is-smaller-or-equal'
+}
+
+export interface Token {
+    type: TokenType;
+    value: any;
+    line: number;
+    col: number;
+}
