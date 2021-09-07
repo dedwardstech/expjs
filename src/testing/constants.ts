3513d25062f3e2df1ae9d782cf07078e39c2eab9
David Edwards <davidedwards.tech16@gmail.com>
2021-09-07 18:06:01 -0400

    added testing directory for test constants/functions

---
+++ src/testing/constants.ts
@@ -0,0 +1,6 @@
+export const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+export const alphabetArr = alphabet.split('');
+
+export const digitStr = '0123456789';
+export const digitStrArr = digitStr.split('');
+export const digits = digitStrArr.map(d => parseInt(d, 10));
